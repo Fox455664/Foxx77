fb859202425ebd4a2526c68c402beec873dd76ce
Fox455664 <118382354+Fox455664@users.noreply.github.com>
2024-05-16 15:47:41 +0300

    Update آيه قصيره.py

--- WWWL5/plugins/آيه قصيره.py
+++ WWWL5/plugins/آيه قصيره.py
@@ -1,4 +1,4 @@
-#الملف بحقوق سورس سبايدر @EE_20
+#الملف بحقوق سورس سبايدر @vzo_a
 import asyncio
 import random
 from asyncio.exceptions import TimeoutError
@@ -13,13 +13,13 @@ from ..helpers.utils import reply_id
 @WWWL5.on(admin_cmd(outgoing=True, pattern="آيه قصيره$"))
 async def jepvois(vois):
   rl = random.randint(111,210)
-  url = f"https://t.me/MEMESv2/{rl}"
-  await vois.client.send_file(vois.chat_id,url,caption="⎊︙ BY : @EE_20 🌺",parse_mode="html")
+  url = f"https://t.me/Fox455664/{rl}"
+  await vois.client.send_file(vois.chat_id,url,caption="⎊︙ BY : @vzo_a 🌺",parse_mode="html")
   await vois.delete()
 
 
 @WWWL5.on(admin_cmd(outgoing=True, pattern="سوره الفاتحة$"))
 async def jepvois(Voice):
-  url = f"https://t.me/EE_SPID/26"
-  await Voice.client.send_file(Voice.chat_id,url,caption="⎊︙ سوره الفاتحة\n⎊︙ بصوت القارئ ماهر المعيقلي\n⎊︙ BY : @EE_20 🌺",parse_mode="html")
+  url = f"https://t.me/vzo_a"
+  await Voice.client.send_file(Voice.chat_id,url,caption="⎊︙ سوره الفاتحة\n⎊︙ بصوت القارئ ماهر المعيقلي\n⎊︙ BY : @vzo_a 🌺",parse_mode="html")
   await Voice.delete()
